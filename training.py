508874439c0b9d718b819d91d8201f35ab9bec72
Aaditya Waghulade <aasityaprashantwaghulade2004@gmail.com>
2025-06-03 12:55:55 +0530

    B) Lemmatizing the word by WordNetLemmatizer()
    First step lemmatizing the words which are not in ignore_letter list and lemmatized them.

--- training.py
+++ training.py
@@ -9,6 +9,7 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 nltk.download('punkt_tab')
+nltk.download('wordnet')
 #3. Tensorflow-keras for layers , activation function for building neural network
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout , Activation
@@ -17,7 +18,7 @@ from tensorflow.keras.optimizers import SGD
 #SGD : Stochastic Gradient Descent is an optimization algorithm used to minimize the loss function and update the model parameters.
 #"Stochastic" means we don’t use all data at once. We use just one data point or a small batch at a time. This makes it faster and works well for big datasets.
 
-lemmatizer = WordNetLemmatizer()
+# A) LOADING DATA
 
 #4.Opening the "intents.json" file
 with open("intents.json") as intents_json:
@@ -36,10 +37,19 @@ ignore_letters = ['?',',','.','!']#for storing some letters which are not really
 for intent in intents['intents']: # 1.intents = json.loads(data) 2. intents['intents'] =  'intents.json'==>{intents :[]}
     for pattern in intent["patterns"]:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
+        words.extend(word_list) #extend() means taking the content in the list and append() means adding the another list in a list
         documents.append((word_list,intent['tag'])) #document =[] will contain every word and class which are intent:tag in intents.jsone
         if  intent['tag'] not in classes:
             classes.append(intent['tag'])
+#print(documents)
 
+#LOADING DATA IS DONE
 
-print(documents)
+# B) TRAINING THE DATA
+#6. Lemmatizing the words
+lemmatizer = WordNetLemmatizer()
+for word in words:
+    if word not in ignore_letters:
+        words = [lemmatizer.lemmatize(word)]
+#Lemmatizing the each word by iterating the words list because it is now sorted. then entering in list words and the word is not matching with ignoring letters then add that word in words[] by using lemmatizer
+print(words)
