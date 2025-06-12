a54cf8078f6032555f001f364b23511fe4048177
Aaditya Waghulade <aasityaprashantwaghulade2004@gmail.com>
2025-06-12 17:51:06 +0530

    Neural Network Training Error solving

--- training.py
+++ training.py
@@ -7,8 +7,8 @@ import numpy as np
 #2. nlp libs for Training data
 import nltk
 from nltk.stem import WordNetLemmatizer
-nltk.download('punkt_tab')
-nltk.download('wordnet')
+#nltk.download('punkt_tab')
+#nltk.download('wordnet')
 #3. Tensorflow-keras for layers , activation function for building neural network
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout , Activation
@@ -98,7 +98,14 @@ train_y = list(training[:,1])
 model = Sequential()
 model.add(Dense(128, input_shape=len(train_x[0]),activation='relu'))#Feeding Bag of words to the model which is our 'train_x'
 model.add(Dropout(0.5))
-model.add(Dense(64), acitvation = 'relu')
+model.add(Dense(64, acitvation = 'relu'))
 model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]),activation = 'softmax'))
 
+sgd = SGD(lr = 0.01,decay =1e-6,momentum=0.9,nesterov =True)
 
+model.compile(loss='categorical_crossentropy',optimizer = sgd , metrics = ['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y),epochs=200, batch_size=5,verbose = 1)
+model.save('chatbot_model.model')
+print("done")
