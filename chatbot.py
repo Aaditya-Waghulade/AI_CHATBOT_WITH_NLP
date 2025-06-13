2d237bc178bd6dd59be0b8012514d07f6f9e8052
Aaditya Waghulade <aasityaprashantwaghulade2004@gmail.com>
2025-06-13 19:17:46 +0530

    Updating Chatbot.py
    Created Functions for
    1. Tokenizing and lemmatizing user input
    2. Creating bag of words to check whether the word is available there or not

--- chatbot.py
+++ chatbot.py
@@ -13,5 +13,12 @@ words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 model = load_model('chatbot_model.h5')
 
+#FLOW OF THE PROGRAM
+#1. TOKENNIZING AND LEMMATIZING USER INPUT
+def tokenizing_input(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words ]
+    return sentence_words
+ 
 
-
+#2. NOW CREATING SENTENCE INTO BAG OF WORDS (List of 1 and 0 which will indicates whether that word is Avaialble in the Bag Or Not)
